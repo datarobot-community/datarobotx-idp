deb5cfe0ef963ed321927b32d2a29c90a83c42ba
Marcus <Marcus.braun@datarobot.com>
2024-04-23 10:24:45 -0500

    playgrounds helper (#8)

---
+++ tests/integration/test_playgrounds.py
@@ -0,0 +1,59 @@
+#
+# Copyright 2024 DataRobot, Inc. and its affiliates.
+#
+# All rights reserved.
+#
+# DataRobot, Inc.
+#
+# This is proprietary source code of DataRobot, Inc. and its
+# affiliates.
+#
+# Released under the terms of DataRobot Tool and Utility Agreement.
+# https://www.datarobot.com/wp-content/uploads/2021/07/DataRobot-Tool-and-Utility-Agreement.pdf
+
+import pytest
+
+from datarobotx.idp.playgrounds import get_or_create_playground
+from datarobotx.idp.use_cases import get_or_create_use_case
+
+
+@pytest.fixture
+def use_case(dr_endpoint, dr_token, cleanup_dr):
+    with cleanup_dr("useCases/"):
+        yield get_or_create_use_case(
+            dr_endpoint,
+            dr_token,
+            "pytest use case",
+        )
+
+
+@pytest.fixture
+def cleanup_env(cleanup_dr):
+    with cleanup_dr("genai/playgrounds/"):
+        yield
+
+
+def test_get_or_create(dr_endpoint, dr_token, use_case, cleanup_env):
+    pg_1 = get_or_create_playground(
+        dr_endpoint,
+        dr_token,
+        "pytest playground #1",
+        use_case=use_case,
+    )
+    assert len(pg_1)
+
+    pg_2 = get_or_create_playground(
+        dr_endpoint,
+        dr_token,
+        "pytest playground #1",
+        use_case=use_case,
+    )
+    assert pg_1 == pg_2
+
+    pg_3 = get_or_create_playground(
+        dr_endpoint,
+        dr_token,
+        "pytest playground #2",
+        use_case=use_case,
+    )
+    assert pg_1 != pg_3
