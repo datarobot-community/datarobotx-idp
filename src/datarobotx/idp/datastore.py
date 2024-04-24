4eafd012ad33ee46b93b186eb2ab413e71686a60
Marcus Braun <marcus.braun@datarobot.com>
2024-04-24 14:52:50 -0500

    add copyright, more precise type ignores

--- src/datarobotx/idp/datastore.py
+++ src/datarobotx/idp/datastore.py
@@ -1,5 +1,16 @@
-# mypy: disable-error-code="attr-defined"
-# pyright: reportPrivateImportUsage=false
+#
+# Copyright 2024 DataRobot, Inc. and its affiliates.
+#
+# All rights reserved.
+#
+# DataRobot, Inc.
+#
+# This is proprietary source code of DataRobot, Inc. and its
+# affiliates.
+#
+# Released under the terms of DataRobot Tool and Utility Agreement.
+# https://www.datarobot.com/wp-content/uploads/2021/07/DataRobot-Tool-and-Utility-Agreement.pdf
+
 
 from typing import Any
 
@@ -9,7 +20,7 @@ import datarobot as dr
 
 
 def _find_existing_datastore(canonical_name: str) -> str:
-    dss = dr.DataStore.list()
+    dss = dr.DataStore.list()  # type: ignore
     datastore = [ds for ds in dss if ds.canonical_name and ds.canonical_name == canonical_name][0]
     return str(datastore.id)
 
@@ -30,18 +41,18 @@ def get_or_create_datastore(
     Records a checksum in the datasource name to allow future calls to this
     function to validate whether a desired datasource already exists
     """
-    dr.Client(token=token, endpoint=endpoint)
+    dr.Client(token=token, endpoint=endpoint)  # type: ignore
 
     datastore_token = get_hash(canonical_name, driver_id, jdbc_url, data_store_type, **kwargs)
 
     canonical_name = f"{canonical_name} [{datastore_token}]"
     try:
-        datastore = dr.DataStore.get(_find_existing_datastore(canonical_name))
+        datastore = dr.DataStore.get(_find_existing_datastore(canonical_name))  # type: ignore
         return str(datastore.id)
     except IndexError:
         pass
 
-    datastore = dr.DataStore.create(
+    datastore = dr.DataStore.create(  # type: ignore
         data_store_type=data_store_type,
         canonical_name=canonical_name,
         driver_id=driver_id,
