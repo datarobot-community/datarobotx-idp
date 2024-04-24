54a4991d7e649ffcab8524a795ba0a86061f63ed
Marcus Braun <marcus.braun@datarobot.com>
2024-04-24 15:49:03 -0500

    add copyrights, more targeted mypy overrides

--- tests/integration/test_datasource.py
+++ tests/integration/test_datasource.py
@@ -1,3 +1,16 @@
+#
+# Copyright 2024 DataRobot, Inc. and its affiliates.
+#
+# All rights reserved.
+#
+# DataRobot, Inc.
+#
+# This is proprietary source code of DataRobot, Inc. and its
+# affiliates.
+#
+# Released under the terms of DataRobot Tool and Utility Agreement.
+# https://www.datarobot.com/wp-content/uploads/2021/07/DataRobot-Tool-and-Utility-Agreement.pdf
+
 import pytest
 
 from datarobotx.idp.datasource import get_or_create_datasource
