e3907f0f73483b14dd4aa431226dc84cd3c61534
derluke <lukas.innig@gmail.com>
2024-04-30 06:49:47 +0100

    custom model create check to avoid configuration error  (#20)
    * change check to be more precise
    
    * changed to use dr.CustomInferenceModel
    
    * typing
    
    ---------
    
    Co-authored-by: Lukas Innig <lukas.innig@datarobot.com>

--- src/datarobotx/idp/custom_models.py
+++ src/datarobotx/idp/custom_models.py
@@ -13,36 +13,9 @@
 
 # mypy: disable-error-code="attr-defined"
 # pyright: reportPrivateImportUsage=false
-import posixpath
-from typing import Any
-
-import requests
+from typing import Any, Union
 
 import datarobot as dr
-from datarobot.rest import handle_http_error
-from datarobot.utils import camelize
-
-
-def _create_custom_model(
-    endpoint: str,
-    token: str,
-    name: str,
-    target_type: str,
-    custom_model_type: str = "inference",
-    **kwargs: Any,
-) -> str:
-    url = posixpath.join(endpoint, "customModels/")
-    body = {
-        "name": name,
-        "target_type": target_type,
-        "custom_model_type": custom_model_type,
-    }
-    body.update(kwargs)
-    body = {camelize(k): v for k, v in body.items()}
-    resp = requests.post(url, headers={"Authorization": f"Bearer {token}"}, json=body)
-    if not resp:
-        handle_http_error(resp)
-    return str(resp.json()["id"])
 
 
 def _find_existing_custom_model(**kwargs: Any) -> str:
@@ -53,11 +26,18 @@ def _find_existing_custom_model(**kwargs: Any) -> str:
 
 
 def get_or_create_custom_model(
-    endpoint: str, token: str, name: str, target_type: str, **kwargs: Any
+    endpoint: str,
+    token: str,
+    name: str,
+    target_type: Union[str, dr.enums.TARGET_TYPE],
+    **kwargs: Any,
 ) -> str:
     """Get or create a custom model with requested parameters."""
     dr.Client(endpoint=endpoint, token=token)
+
     try:
         return _find_existing_custom_model(name=name, target_type=target_type, **kwargs)
     except KeyError:
-        return _create_custom_model(endpoint, token, name, target_type, **kwargs)
+        return str(
+            dr.CustomInferenceModel.create(name=name, target_type=target_type, **kwargs).id  # type: ignore[arg-type]
+        )
