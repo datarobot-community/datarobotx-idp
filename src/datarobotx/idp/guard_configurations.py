e39aec1c194c0ea86812a437680bf29bdb233648
Lukas Innig <lukas.innig@datarobot.com>
2024-05-26 23:05:11 +0100

    support guard templates

---
+++ src/datarobotx/idp/guard_configurations.py
@@ -0,0 +1,160 @@
+# pyright: reportPrivateImportUsage=false
+
+from typing import Any, Dict, List, Optional
+
+import datarobot as dr
+from datarobot.errors import ClientError
+from datarobot.utils import from_api, to_api
+
+from datarobotx.idp.common.hashing import get_hash
+from datarobotx.idp.custom_model_versions import (
+    get_or_create_custom_model_version_from_previous,
+)
+
+
+def _clean_guard_configurations(guard_config: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+    cleaned_config = []
+    for config in guard_config:
+        new_config = {}
+        for k, v in config.items():
+            if k in [
+                "created_at",
+                "creator_id",
+                "entity_id",
+                "entity_type",
+                "org_id",
+                "allowed_stages",
+                "additional_config",
+                "production_only",
+                "id",
+            ]:
+                continue
+            if v is not None:
+                new_config[k] = v
+        cleaned_config.append(new_config)
+    return cleaned_config
+
+
+def ensure_guard_config_from_template(  # noqa: PLR0913
+    endpoint: str,
+    token: str,
+    custom_model_id: str,
+    guard_config_template_name: str,
+    guard_config_template_settings: Dict[str, Any],
+    stages: List[str],
+    intervention: Dict[str, Any],
+    name: Optional[str] = None,
+) -> str:
+    """Ensure a guard configuration exists for a custom model version.
+
+    Parameters
+    ----------
+    endpoint : str
+        The DataRobot endpoint to connect to.
+    token : str
+        The DataRobot API token to use.
+    custom_model_id : str
+        The ID of the custom model to create the guard configuration for.
+    guard_config_template_name : str
+        The name of the guard configuration template to use.
+    guard_config_template_settings : dict
+        The settings for the guard configuration template.
+    stages : Union[List[GuardStage], List[str]]
+        The stages to apply the guard configuration to.
+    intervention : Union[Intervention, Dict[str, Any]]
+        The intervention to apply when the guard conditions are met.
+    name : Optional[str], optional
+        The name of the guard configuration, by default None
+
+    Returns
+    -------
+    str
+        The ID of the custom model version with the guard configuration.
+    """
+    if endpoint is not None and token is not None:
+        client = dr.Client(endpoint=endpoint, token=token)  # type: ignore
+    else:
+        client = dr.client.get_client()
+
+    guard_token = get_hash(
+        custom_model_id,
+        guard_config_template_name,
+        stages,
+        intervention,
+        guard_config_template_settings,
+        name,
+    )
+
+    guard_name = f"{name or guard_config_template_name} - [{guard_token}]"
+
+    custom_model = dr.CustomInferenceModel.get(custom_model_id)  # type: ignore
+    if not custom_model.latest_version:
+        raise ValueError("Custom model has no versions")
+    latest_version_id = custom_model.latest_version.id
+
+    # we extract the base environment id from the latest version to be reused later
+    base_environment_id = custom_model.latest_version.base_environment_id
+
+    # get the current guard configurations
+    guard_config = client.get(
+        "guardConfigurations/",
+        params={"entityId": latest_version_id, "entityType": "customModelVersion"},
+    ).json()["data"]
+
+    guard_config = [from_api(config) for config in guard_config]
+
+    # check if the guard configuration already exists
+    if guard_config:
+        for config in guard_config:
+            if config["name"] == guard_name:
+                return str(latest_version_id)
+
+    cleaned_guard_config = _clean_guard_configurations(guard_config)
+
+    # get the guard templates
+    guard_templates = client.get("guardTemplates/").json()["data"]
+    guard_templates = [
+        from_api(gt) for gt in guard_templates if gt["name"] == guard_config_template_name
+    ]
+
+    if not guard_templates:
+        raise ValueError(f"Guard template {guard_config_template_name} not found")
+
+    selected_template = guard_templates[0]
+    if not all([stage in selected_template["allowed_stages"] for stage in stages]):
+        raise ValueError(
+            f"Guard template {guard_config_template_name} does not support stages {stages}"
+        )
+
+    # Assemble the guard configuration with all expected fields
+    cleaned_guard_template = _clean_guard_configurations([selected_template])[0]
+    cleaned_guard_template["stages"] = [stage for stage in stages]
+    cleaned_guard_template["intervention"] = intervention
+    cleaned_guard_template.update(guard_config_template_settings)
+    cleaned_guard_template["name"] = guard_name
+
+    # append previous guard configurations
+    cleaned_guard_config.append(cleaned_guard_template)
+    try:
+        res = client.post(
+            "guardConfigurations/toNewCustomModelVersion/",
+            json={
+                "data": [to_api(config) for config in cleaned_guard_config],
+                "customModelId": custom_model_id,
+            },
+        )
+    except ClientError:
+        _ = get_or_create_custom_model_version_from_previous(
+            endpoint=endpoint,
+            token=token,
+            custom_model_id=custom_model_id,
+            base_environment_id=base_environment_id,
+        )
+        res = client.post(
+            "guardConfigurations/toNewCustomModelVersion/",
+            json={
+                "data": [to_api(config) for config in cleaned_guard_config],
+                "customModelId": custom_model_id,
+            },
+        )
+    return str(res.json()["customModelVersionId"])
---
+++ tests/integration/test_guard_configurations.py
@@ -0,0 +1,185 @@
+from typing import Any, Generator
+
+import pytest
+
+import datarobot as dr
+from datarobotx.idp.custom_model_versions import get_or_create_custom_model_version  # type: ignore
+from datarobotx.idp.custom_models import get_or_create_custom_model  # type: ignore
+from datarobotx.idp.deployments import (  # type: ignore
+    get_or_create_deployment_from_registered_model_version,
+)
+from datarobotx.idp.guard_configurations import ensure_guard_config_from_template  # type: ignore
+
+
+@pytest.fixture
+def custom_model_id(dr_endpoint: str, dr_token: str, cleanup_dr: Any) -> Generator[str, None, None]:
+    with cleanup_dr("customModels"):
+        custom_model = get_or_create_custom_model(
+            endpoint=dr_endpoint,
+            token=dr_token,
+            name="pytest Custom Model",
+            target_type="TextGeneration",
+            target_name="text",
+        )
+        yield custom_model
+
+
+@pytest.fixture
+def custom_model_version_id(
+    dr_endpoint: str, dr_token: str, custom_model_id: str, cleanup_dr: Any
+) -> Generator[str, None, None]:
+    with cleanup_dr("customModels"):
+        custom_model = get_or_create_custom_model_version(
+            endpoint=dr_endpoint,
+            token=dr_token,
+            custom_model_id=custom_model_id,
+            base_environment_id="65f9b27eab986d30d4c64268",
+        )
+        yield custom_model
+
+
+@pytest.fixture
+def guard_deployment_toxicity(
+    dr_endpoint: str, dr_token: str, default_prediction_server_id: str, cleanup_dr: Any
+) -> Generator[str, None, None]:
+    registered_model_name = "Toxicity Classifier"
+    deployment_name = "Toxicity Deployment pytest"
+
+    with cleanup_dr("deployments"):
+        for version in dr.RegisteredModel.list(search=registered_model_name)[0].list_versions():  # type: ignore
+            if version.build_status == "failed":
+                continue
+            registered_model_vers_id = version.id
+            break
+        deployment = get_or_create_deployment_from_registered_model_version(
+            endpoint=dr_endpoint,
+            token=dr_token,
+            registered_model_version_id=registered_model_vers_id,
+            label=deployment_name,
+            default_prediction_server_id=default_prediction_server_id,
+        )
+        yield deployment
+
+
+@pytest.fixture
+def guard_template_name_toxicity() -> str:
+    return "Toxicity"
+
+
+@pytest.fixture
+def guard_deployment_prompt_injection(
+    dr_endpoint: str, dr_token: str, default_prediction_server_id: str, cleanup_dr: Any
+) -> Generator[str, None, None]:
+    registered_model_name = "Prompt Injection Classifier"
+    deployment_name = "Prompt Injection Deployment pytest"
+
+    with cleanup_dr("deployments"):
+        for version in dr.RegisteredModel.list(search=registered_model_name)[0].list_versions():  # type: ignore
+            if version.build_status == "failed":
+                continue
+            registered_model_vers_id = version.id
+            break
+        deployment = get_or_create_deployment_from_registered_model_version(
+            endpoint=dr_endpoint,
+            token=dr_token,
+            registered_model_version_id=registered_model_vers_id,
+            label=deployment_name,
+            default_prediction_server_id=default_prediction_server_id,
+        )
+        yield deployment
+
+
+@pytest.fixture
+def guard_template_name_prompt_injection() -> str:
+    return "Prompt Injection"
+
+
+def test_ensure_guard_config_from_template(
+    dr_endpoint: str,
+    dr_token: str,
+    custom_model_id: str,
+    guard_template_name_toxicity: str,
+    guard_deployment_toxicity: str,
+    guard_template_name_prompt_injection: str,
+    guard_deployment_prompt_injection: str,
+    custom_model_version_id: str,
+) -> None:
+    # Call the function
+    assert custom_model_version_id
+    latest_version = ensure_guard_config_from_template(
+        endpoint=dr_endpoint,
+        token=dr_token,
+        custom_model_id=custom_model_id,
+        guard_config_template_name=guard_template_name_toxicity,
+        guard_config_template_settings={
+            "deploymentId": guard_deployment_toxicity,
+        },
+        stages=["prompt"],
+        intervention={
+            "action": "block",
+            "message": "This is a toxic message.",
+            "conditions": [{"comparand": 0.5, "comparator": "greaterThan"}],
+            "send_notification": True,
+        },
+        name="pytest Toxicity Guard Configuration",
+    )
+
+    # Check the result
+    client = dr.Client(endpoint=dr_endpoint, token=dr_token)  # type: ignore
+    guard_config = client.get(
+        "guardConfigurations/",
+        params={"entityId": latest_version, "entityType": "customModelVersion"},
+    ).json()["data"]
+    assert guard_config
+    assert any(["Toxicity" in gc["name"] for gc in guard_config])
+
+    latest_version_2 = ensure_guard_config_from_template(
+        endpoint=dr_endpoint,
+        token=dr_token,
+        custom_model_id=custom_model_id,
+        guard_config_template_name=guard_template_name_toxicity,
+        guard_config_template_settings={
+            "deploymentId": guard_deployment_toxicity,
+        },
+        stages=["prompt"],
+        intervention={
+            "action": "block",
+            "message": "This is a toxic message.",
+            "conditions": [{"comparand": 0.5, "comparator": "greaterThan"}],
+            "send_notification": True,
+        },
+        name="pytest Toxicity Guard Configuration",
+    )
+
+    assert latest_version == latest_version_2
+
+    latest_version_3 = ensure_guard_config_from_template(
+        endpoint=dr_endpoint,
+        token=dr_token,
+        custom_model_id=custom_model_id,
+        guard_config_template_name=guard_template_name_prompt_injection,
+        guard_config_template_settings={
+            "deploymentId": guard_deployment_prompt_injection,
+        },
+        stages=["prompt"],
+        intervention={
+            "action": "block",
+            "message": "This is a prompt injection message.",
+            "conditions": [{"comparand": 0.5, "comparator": "greaterThan"}],
+            "send_notification": True,
+        },
+        name="pytest Prompt Injection Guard Configuration",
+    )
+
+    # Check the result
+    client = dr.Client(endpoint=dr_endpoint, token=dr_token)  # type: ignore
+    guard_config = client.get(
+        "guardConfigurations/",
+        params={"entityId": latest_version_3, "entityType": "customModelVersion"},
+    ).json()["data"]
+    assert guard_config
+    assert any(["Prompt Injection" in gc["name"] for gc in guard_config]) and any(
+        ["Toxicity" in gc["name"] for gc in guard_config]
+    )
+
+    assert latest_version != latest_version_3
