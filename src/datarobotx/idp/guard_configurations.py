94be304469fae0db29edd30e416d38e2af6ffa07
Lukas Innig <lukas.innig@datarobot.com>
2024-05-26 23:11:32 +0100

    licenses

--- src/datarobotx/idp/guard_configurations.py
+++ src/datarobotx/idp/guard_configurations.py
@@ -1,3 +1,16 @@
+# Copyright 2024 DataRobot, Inc. and its affiliates.
+#
+# All rights reserved.
+#
+# DataRobot, Inc.
+#
+# This is proprietary source code of DataRobot, Inc. and its
+# affiliates.
+#
+# Released under the terms of DataRobot Tool and Utility Agreement.
+# https://www.datarobot.com/wp-content/uploads/2021/07/DataRobot-Tool-and-Utility-Agreement.pdf
+
+# mypy: disable-error-code="attr-defined"
 # pyright: reportPrivateImportUsage=false
 
 from typing import Any, Dict, List, Optional
