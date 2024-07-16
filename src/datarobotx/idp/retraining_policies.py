c7b6a5f87afad09281d2d719ecd97386549bb76f
John Eastman <john.eastman@datarobot.com>
2024-07-16 15:33:14 -0400

    made Marcus and Marshall's suggested changes

--- src/datarobotx/idp/retraining_policies.py
+++ src/datarobotx/idp/retraining_policies.py
@@ -11,17 +11,61 @@
 # https://www.datarobot.com/wp-content/uploads/2021/07/DataRobot-Tool-and-Utility-Agreement.pdf
 
 import json
-from typing import Any, Dict, Union
+from typing import Any, Union
 
 import datarobot as dr
 
 
 def _check_response(response: Any) -> None:
-    if response.status_code < 200 or response.status_code > 399:
+    if response.status_code < 200 or response.status_code > 299:
         raise Exception(f"Request failed with status code {response.status_code}: {response.text}")
 
 
-def update_or_create_retraining_policy(
+def _configure_retraining_settings(dataset_id: str, deployment_id: str, client: Any) -> None:
+    deployment = dr.Deployment.get(deployment_id=deployment_id)  # type: ignore
+    prediction_env_id = deployment.default_prediction_server["id"]  # type: ignore
+
+    get_payload = {"datasetId": dataset_id, "predictionEnvironmentId": prediction_env_id}
+
+    settings_response = client.request(
+        method="PATCH", url=f"deployments/{deployment_id}/retrainingSettings", json=get_payload
+    )
+    _check_response(settings_response)
+
+
+def _search_for_existing_policy(client: Any, deployment_id: str, policy_name: str) -> Any:
+    get_response = client.request(
+        method="GET", url=f"deployments/{deployment_id}/retrainingPolicies"
+    )
+    retraining_policies = json.loads(get_response.text)["data"]
+
+    for policy in retraining_policies:
+        if policy["name"] == policy_name:
+            return policy["id"]
+    return ""
+
+
+def _post(client: Any, deployment_id: str, payload: Any) -> Any:
+    response = client.request(
+        method="POST",
+        url=f"deployments/{deployment_id}/retrainingPolicies",
+        json=payload,
+    )
+    _check_response(response)
+    return json.loads(response.text)["id"]
+
+
+def _patch(client: Any, deployment_id: str, payload: Any, retraining_policy_id: str) -> None:
+    response = client.request(
+        method="PATCH",
+        url=f"deployments/{deployment_id}/retrainingPolicies/{retraining_policy_id}",
+        json=payload,
+    )
+    _check_response(response)
+    return None
+
+
+def get_update_or_create_retraining_policy(
     endpoint: str,
     token: str,
     deployment_id: str,
@@ -33,18 +77,11 @@ def update_or_create_retraining_policy(
 
     Parameters
     ----------
-    endpoint : str
-        The endpoint for the DataRobot API.
-    token : str
-        The token for authentication with the DataRobot API.
-    deployment_id : str
-        The ID of the deployment to update or create the retraining policy for.
-    name : str
-        The name of the retraining policy.
     dataset_id : Union[str, None], optional
         The ID of the dataset used for retraining, by default None.
     **kwargs : Any
         Additional keyword arguments for the retraining policy.
+        Reference: https://docs.datarobot.com/en/docs/api/reference/public-api/deployments.html#patch-apiv2deploymentsdeploymentidretrainingpoliciesretrainingpolicyid
 
     Returns
     -------
@@ -53,45 +90,17 @@ def update_or_create_retraining_policy(
     """
     client = dr.Client(token=token, endpoint=endpoint)  # type: ignore
 
-    # Configure retraining settings
-    if dataset_id is not None:
-        deployment = dr.Deployment.get(deployment_id=deployment_id)  # type: ignore
-        prediction_env_id = deployment.default_prediction_server["id"]  # type: ignore
-        get_payload = {"datasetId": dataset_id, "predictionEnvironmentId": prediction_env_id}
-        settings_response = client.request(
-            method="PATCH", url=f"deployments/{deployment_id}/retrainingSettings", json=get_payload
-        )
-        _check_response(settings_response)
+    if dataset_id:
+        _configure_retraining_settings(dataset_id, deployment_id, client)
 
-    get_response = client.request(
-        method="GET", url=f"deployments/{deployment_id}/retrainingPolicies"
-    )
-    retraining_policies_payload: Dict[str, Any] = json.loads(get_response.text)
-    policies = retraining_policies_payload["data"]
+    policy_payload_to_upload = {"name": name, **kwargs}
 
-    policy_payload_update = {"name": name, **kwargs}
-    response: Any = None
-    retraining_policy_id: str = ""
+    retraining_policy_id: str = _search_for_existing_policy(client, deployment_id, name)
 
-    # If there is an existing policy w/ inputted name, PATCH, otherwise POST
-    for policy in policies:
-        if policy["name"] == name:
-            retraining_policy_id = policy["id"]
-            response = client.request(
-                method="PATCH",
-                url=f"deployments/{deployment_id}/retrainingPolicies/{retraining_policy_id}",
-                json=policy_payload_update,
-            )
-            print(json.loads(response.text))
-            break
-
-    if response is None:
-        response = client.request(
-            method="POST",
-            url=f"deployments/{deployment_id}/retrainingPolicies",
-            json=policy_payload_update,
-        )
-        retraining_policy_id = json.loads(response.text)["id"]
-        print(json.loads(response.text))
+    # If there is no existing policy w/ inputted name, POST, otherwise PATCH
+    if retraining_policy_id == "":
+        retraining_policy_id = _post(client, deployment_id, policy_payload_to_upload)
+    else:
+        _patch(client, deployment_id, policy_payload_to_upload, retraining_policy_id)
 
     return retraining_policy_id
--- tests/integration/test_retraining_policies.py
+++ tests/integration/test_retraining_policies.py
@@ -11,16 +11,19 @@
 # Released under the terms of DataRobot Tool and Utility Agreement.
 # https://www.datarobot.com/wp-content/uploads/2021/07/DataRobot-Tool-and-Utility-Agreement.pdf
 
+import json
+
 import pandas as pd
 import pytest
 
 import datarobot as dr
 from datarobotx.idp.autopilot import get_or_create_autopilot_run
 from datarobotx.idp.datasets import get_or_create_dataset_from_df
+from datarobotx.idp.deployments import get_or_create_deployment_from_registered_model_version
 from datarobotx.idp.registered_model_versions import (
     get_or_create_registered_leaderboard_model_version,
 )
-from datarobotx.idp.retraining_policies import update_or_create_retraining_policy
+from datarobotx.idp.retraining_policies import get_update_or_create_retraining_policy
 from datarobotx.idp.use_cases import get_or_create_use_case
 
 
@@ -108,37 +111,84 @@ def registered_model_version(
 
 @pytest.fixture
 def deployment_id(
-    cleanup_dr, registered_model_version, registered_model_name, default_prediction_server_id
+    cleanup_dr,
+    registered_model_version,
+    registered_model_name,
+    default_prediction_server_id,
+    dr_endpoint,
+    dr_token,
 ):
     with cleanup_dr("deployments/"):
-        yield dr.Deployment.create_from_registered_model_version(
-            model_package_id=registered_model_version,
+        yield get_or_create_deployment_from_registered_model_version(
+            endpoint=dr_endpoint,
+            token=dr_token,
+            registered_model_version_id=registered_model_version,
             label=registered_model_name,
             default_prediction_server_id=default_prediction_server_id,
-        ).id
+        )
 
 
 def test_retraining_policy(dr_endpoint, dr_token, deployment_id, dataset):
-    retraining_id_1 = update_or_create_retraining_policy(
+    print(deployment_id)
+    # Can we create policy
+    retraining_id_1 = get_update_or_create_retraining_policy(
         endpoint=dr_endpoint,
         token=dr_token,
         deployment_id=deployment_id,
-        name="test",
+        name="post_test",
         dataset_id=dataset,
     )
-    retraining_id_2 = update_or_create_retraining_policy(
+
+    # If we "update" policy, is the ID the same?
+    retraining_id_2 = get_update_or_create_retraining_policy(
         endpoint=dr_endpoint,
         token=dr_token,
         deployment_id=deployment_id,
-        name="test",
+        name="post_test",
         dataset_id=dataset,
     )
     assert retraining_id_1 == retraining_id_2
-    retraining_id_3 = update_or_create_retraining_policy(
+
+    # If we actually change a parameter, is the ID the same?
+    retraining_id_3 = get_update_or_create_retraining_policy(
         endpoint=dr_endpoint,
         token=dr_token,
         deployment_id=deployment_id,
-        name="test2",
+        name="patch_test",
+        dataset_id=dataset,
+        featureListStrategy="informative_features",
+    )
+
+    # Add test to assert the update occurred
+    client = dr.Client(token=dr_token, endpoint=dr_endpoint)
+    get_response = client.request(
+        method="GET", url=f"deployments/{deployment_id}/retrainingPolicies/{retraining_id_3}"
+    )
+    policy_data = json.loads(get_response.text)
+    assert policy_data["featureListStrategy"] == "informative_features"
+
+    retraining_id = get_update_or_create_retraining_policy(
+        endpoint=dr_endpoint,
+        token=dr_token,
+        deployment_id=deployment_id,
+        name="test",
         dataset_id=dataset,
     )
-    assert retraining_id_3 != retraining_id_2
+
+    retraining_response_1 = client.get(
+        f"deployments/{deployment_id}/retrainingPolicies/{retraining_id}"
+    ).json()
+
+    retraining_id_2 = get_update_or_create_retraining_policy(
+        endpoint=dr_endpoint,
+        token=dr_token,
+        deployment_id=deployment_id,
+        name="test",
+        dataset_id=dataset,
+    )
+
+    retraining_response_2 = client.get(
+        f"deployments/{deployment_id}/retrainingPolicies/{retraining_id_2}"
+    ).json()
+
+    assert retraining_response_1 == retraining_response_2
