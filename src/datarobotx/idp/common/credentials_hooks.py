c1268f09660da80ba281ac65cc36bf1acd72f6d0
Marshall Krassenstein <mpkrass@gmail.com>
2024-08-28 10:30:10 -0400

    Inject DR Credentials implementation
    Lint
    
    Remove needless init and wordsmith

--- src/datarobotx/idp/common/credentials_hooks.py
+++ src/datarobotx/idp/common/credentials_hooks.py
@@ -11,6 +11,7 @@
 # Released under the terms of DataRobot Tool and Utility Agreement.
 # https://www.datarobot.com/wp-content/uploads/2021/07/DataRobot-Tool-and-Utility-Agreement.pdf
 
+import logging
 from typing import Any, Dict
 
 try:
@@ -25,27 +26,46 @@ from datarobotx.idp.common.handle_io import get_feed_dict
 class CredentialsHooks:
     """Include credentials in the catalog so they can be used by nodes."""
 
+    @property
+    def _logger(self):
+        return logging.getLogger(__name__)
+
     @hook_impl
     def after_catalog_created(
         self,
         catalog: DataCatalog,
-        conf_catalog: Dict[str, Any],
         conf_creds: Dict[str, Any],
-        save_version: str,
-        load_versions: Dict[str, str],
     ) -> None:
         """Validate credentials and add them to the catalog for node consumption."""
-        self.validate_credentials(conf_creds)
+        self._validate_or_replace_credentials(conf_creds)
 
-        creds = get_feed_dict(conf_creds, copy_dict_as=None, key_prefix="params:credentials.")
+        creds = get_feed_dict(
+            conf_creds, copy_dict_as=None, key_prefix="params:credentials."
+        )
         catalog.add_feed_dict(creds)
 
-    @staticmethod
-    def validate_credentials(conf_creds: Dict[str, Any]) -> None:
+    def _validate_or_replace_credentials(self, conf_creds: Dict[str, Any]) -> None:
         """Confirm credentials appear to be valid and functional."""
         import datarobot as dr
 
         dr_creds = conf_creds["datarobot"]
+        try:
+            dr.Client(token=dr_creds["api_token"], endpoint=dr_creds["endpoint"])  # type: ignore
+        except ValueError:
+            self._logger.warning(
+                "Could not authenticate with DataRobot. "
+                "Kedro will run dr.Client() for authentication."
+            )
+            client = dr.Client()
+            conf_creds["datarobot"]["api_token"] = client.token
+            conf_creds["datarobot"]["endpoint"] = client.endpoint
+            prediction_server_id = dr.PredictionServer.list()[0].id
 
-        client = dr.Client(token=dr_creds["api_token"], endpoint=dr_creds["endpoint"])  # type: ignore
-        client.get("version/")
+            if "prediction_environment_id" in dr_creds:
+                conf_creds["datarobot"][
+                    "prediction_environment_id"
+                ] = prediction_server_id
+            elif "default_prediction_server_id" in dr_creds:
+                conf_creds["datarobot"][
+                    "default_prediction_server_id"
+                ] = prediction_server_id
