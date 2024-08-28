39e371b05e0913d6989a43be9bb6566d9c569070
Marshall Krassenstein <mpkrass@gmail.com>
2024-08-28 14:12:49 -0400

    Address change requests
    Restore function signature

--- src/datarobotx/idp/common/credentials_hooks.py
+++ src/datarobotx/idp/common/credentials_hooks.py
@@ -27,14 +27,17 @@ class CredentialsHooks:
     """Include credentials in the catalog so they can be used by nodes."""
 
     @property
-    def _logger(self):
+    def _logger(self) -> logging.Logger:
         return logging.getLogger(__name__)
 
     @hook_impl
     def after_catalog_created(
         self,
         catalog: DataCatalog,
+        conf_catalog: Dict[str, Any],
         conf_creds: Dict[str, Any],
+        save_version: str,
+        load_versions: Dict[str, str],
     ) -> None:
         """Validate credentials and add them to the catalog for node consumption."""
         self._validate_or_replace_credentials(conf_creds)
@@ -48,24 +51,41 @@ class CredentialsHooks:
         """Confirm credentials appear to be valid and functional."""
         import datarobot as dr
 
-        dr_creds = conf_creds["datarobot"]
+        exception_occurred = False
         try:
+            dr_creds = conf_creds["datarobot"]
             dr.Client(token=dr_creds["api_token"], endpoint=dr_creds["endpoint"])  # type: ignore
-        except ValueError:
-            self._logger.warning(
-                "Could not authenticate with DataRobot. "
-                "Kedro will run dr.Client() for authentication."
-            )
-            client = dr.Client()
-            conf_creds["datarobot"]["api_token"] = client.token
-            conf_creds["datarobot"]["endpoint"] = client.endpoint
-            prediction_server_id = dr.PredictionServer.list()[0].id
+        except ValueError:  # Invalid or unpopulated credentials
+            exception_occurred = True
+        except KeyError:  # No credentials.yml file
+            exception_occurred = True
+            conf_creds["datarobot"] = {
+                "prediction_environment_id": None,
+            }
 
-            if "prediction_environment_id" in dr_creds:
+        finally:
+            if exception_occurred:
+                self._logger.warning(
+                    "Could not authenticate with DataRobot using "
+                    "credentials specified in `conf/local/credentials.yml`. "
+                    "Trying credentials automatically retrieved by "
+                    "importing datarobot and calling datarobot.Client()."
+                )
+                client = dr.Client()  # type: ignore
+                available_prediction_environments = [
+                    server.id
+                    for server in dr.PredictionEnvironment.list()  # type: ignore
+                ]
+
+                prediction_environment_id = conf_creds["datarobot"].get(
+                    "prediction_environment_id", ""
+                )
+
+                if prediction_environment_id not in available_prediction_environments:
+                    prediction_environment_id = dr.PredictionServer.list()[0].id  # type: ignore
+
+                conf_creds["datarobot"]["api_token"] = client.token
+                conf_creds["datarobot"]["endpoint"] = client.endpoint
                 conf_creds["datarobot"][
                     "prediction_environment_id"
-                ] = prediction_server_id
-            elif "default_prediction_server_id" in dr_creds:
-                conf_creds["datarobot"][
-                    "default_prediction_server_id"
-                ] = prediction_server_id
+                ] = prediction_environment_id
